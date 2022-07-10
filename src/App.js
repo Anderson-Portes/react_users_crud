28640e2512c1a5cd1b740f25113a3027512d8134
Anderson Portes <portsanderson@gmail.com>
2022-07-10 01:50:27 -0300

    App.js updated

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
           getAllUsers()
           setShow(false)
           setUserData(INITIAL_USER_STATE)
-          setIsUpdating({_id:null})
+          setIsUpdating({ _id:null })
         })
     } else {
       axios.post(API_URL, userData)
@@ -57,32 +57,35 @@ function App() {
       })
   }
 
+  const closeModal = () => {
+    setShow(false);
+    setUserData(INITIAL_USER_STATE);
+    setIsUpdating({ _id: null });
+  }
+
   useEffect(getAllUsers, [])
 
   return (
     <div>
-      <Navbar className="border-bottom" expand="lg">
+      <Navbar bg="dark" expand="lg" variant="dark">
         <Container>
           <Navbar.Brand>
-            Navbar
+            Users Management
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="navbar"/>
           <Navbar.Collapse id="navbar" className="justify-content-end">
             <Nav>
-              <Nav.Link>Inicio</Nav.Link>
-              <Nav.Link>Sobre</Nav.Link>
-              <Nav.Link>Contato</Nav.Link>
+              <Button onClick={() => setShow(true)} className="mt-3 mt-md-0">
+                <i className="bi bi-plus-circle me-2"></i>
+                Adicionar
+              </Button>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
       <Container className="py-4">
-        <Button onClick={() => setShow(true)} className="d-block">
-          <i className="bi bi-plus-circle me-2"></i>
-          Adicionar
-        </Button>
         <Modal show={show}>
-          <Modal.Header closeButton onHide={() => setShow(false)}>
+          <Modal.Header closeButton onHide={closeModal}>
             <Modal.Title>{isUpdating._id ? 'Atualizar' : 'Adicionar' } Usuario</Modal.Title>
           </Modal.Header>
           <Modal.Body>
@@ -109,25 +112,29 @@ function App() {
                 onChange={() => setUserData({...userData, genre: "Feminino"})}/>
               <Form.Check type="radio" name="genero" label="Masculino" inline required
                 onChange={() => setUserData({...userData, genre: "Masculino"})}/>
-              <Button type="submit" className="d-block mt-4 w-100">Enviar</Button>
+              <Button type="submit" className="d-block mt-4 w-100">
+                <i className="bi bi-check2-circle me-2"></i>
+                Enviar
+              </Button>
             </Form>
           </Modal.Body>
         </Modal>
         <Row className="py-3">
           {users?.map(item => (
             <Col md={4} key={item._id}>
-              <Card className="mt-3 p-2 border">
+              <Card className="mb-3 shadow">
                 <Card.Img src={item.photoUrl} />
                 <Card.Body>
                   <Card.Title>{item.name}</Card.Title>
                   <Card.Text>{item.description}</Card.Text>
                   <Card.Text className="text-primary">{item.email}</Card.Text>
                   <Card.Text className="text-success">{item.genre}</Card.Text>
-                  <Button variant="danger" onClick={() => deleteUser(item._id)}>
+                  <Button variant="danger" onClick={() => deleteUser(item._id)} className="w-100 mb-1">
                     <i className="bi bi-trash me-2"></i>
                     Excluir
                   </Button>
-                  <Button variant="success" className="ms-2" onClick={() => updateUser(item)}>
+                  <Button variant="success" className="ms-2" onClick={() => updateUser(item)}
+                    className="w-100">
                     <i className="bi bi-pencil me-2"></i>
                     Editar
                   </Button>
@@ -137,6 +144,9 @@ function App() {
           ))}
         </Row>
       </Container>
+      <div className="bg-dark text-white py-3 text-center">
+        <h1>&copy; Anderson Portes - 2022</h1>
+      </div>
     </div>
   );
 }
