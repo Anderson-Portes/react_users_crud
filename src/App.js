e05337d68c590dc23cf369cea104778cf014300c
Anderson Portes <portsanderson@gmail.com>
2022-07-11 00:28:50 -0300

    Card size fix

--- src/App.js
+++ src/App.js
@@ -4,43 +4,43 @@ import axios from "axios";
 
 const API_URL = "https://users-api-nestjs.herokuapp.com/users"
 const INITIAL_USER_STATE = {
-    name: "",
-    email: "",
-    password: "",
-    genre: "",
-    description: "",
-    photoUrl: ""
-  }
+  name: "",
+  email: "",
+  password: "",
+  genre: "",
+  description: "",
+  photoUrl: ""
+}
 
 function App() {
 
   const [show, setShow] = useState(false)
-  const [isUpdating, setIsUpdating] = useState({_id: null})
+  const [isUpdating, setIsUpdating] = useState({ _id: null })
   const [userData, setUserData] = useState(INITIAL_USER_STATE)
   const [users, setUsers] = useState(null)
-  
-  const handleSubmit =  e => {
+
+  const handleSubmit = e => {
     e.preventDefault()
-    if(isUpdating._id) {
-      axios.patch(API_URL+"/"+isUpdating._id, userData)
+    if (isUpdating._id) {
+      axios.patch(API_URL + "/" + isUpdating._id, userData)
         .then(response => {
           getAllUsers()
           setShow(false)
           setUserData(INITIAL_USER_STATE)
-          setIsUpdating({ _id:null })
+          setIsUpdating({ _id: null })
         })
     } else {
       axios.post(API_URL, userData)
-      .then(() => {
-        getAllUsers()
-        setShow(false)
-        setUserData(INITIAL_USER_STATE)
-      })
+        .then(() => {
+          getAllUsers()
+          setShow(false)
+          setUserData(INITIAL_USER_STATE)
+        })
     }
   }
 
   const updateUser = (item) => {
-    setIsUpdating({_id: item._id})
+    setIsUpdating({ _id: item._id })
     setUserData(item)
     setShow(true)
   }
@@ -72,7 +72,7 @@ function App() {
           <Navbar.Brand>
             Users Management
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbar"/>
+          <Navbar.Toggle aria-controls="navbar" />
           <Navbar.Collapse id="navbar" className="justify-content-end">
             <Nav>
               <Button onClick={() => setShow(true)} className="mt-3 mt-md-0">
@@ -86,32 +86,32 @@ function App() {
       <Container className="py-4">
         <Modal show={show}>
           <Modal.Header closeButton onHide={closeModal}>
-            <Modal.Title>{isUpdating._id ? 'Atualizar' : 'Adicionar' } Usuario</Modal.Title>
+            <Modal.Title>{isUpdating._id ? 'Atualizar' : 'Adicionar'} Usuario</Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <Form onSubmit={handleSubmit}>
               <Form.Control type="text" placeholder="Nome..." value={userData.name}
                 onChange={e => setUserData({
-                  ...userData, name: e.target.value 
-                })} className="mb-2" required/>
-              <Form.Control type="email" placeholder="Email..." value={userData.email} 
-                onChange={e => setUserData({...userData, email: e.target.value})} className="mb-2"
-                required/>
+                  ...userData, name: e.target.value
+                })} className="mb-2" required />
+              <Form.Control type="email" placeholder="Email..." value={userData.email}
+                onChange={e => setUserData({ ...userData, email: e.target.value })} className="mb-2"
+                required />
               <Form.Control type="password" placeholder="Senha...." value={userData.password}
-                onChange={e => setUserData({...userData, password: e.target.value})} className="mb-2"
-                required/>
-              <Form.Control as="textarea" placeholder="Descrição..." 
+                onChange={e => setUserData({ ...userData, password: e.target.value })} className="mb-2"
+                required />
+              <Form.Control as="textarea" placeholder="Descrição..."
                 value={userData.description} onChange={e => setUserData({
                   ...userData, description: e.target.value
-                })} className="mb-2" style={{height: '100px'}} required/>
-              <Form.Control type="text" placeholder="Link da foto..." 
-                value={userData.photoUrl}  onChange={e => setUserData({
+                })} className="mb-2" style={{ height: '100px' }} required />
+              <Form.Control type="text" placeholder="Link da foto..."
+                value={userData.photoUrl} onChange={e => setUserData({
                   ...userData, photoUrl: e.target.value
-                })} className="mb-2" required/>
+                })} className="mb-2" required />
               <Form.Check type="radio" name="genero" label="Feminino" inline required
-                onChange={() => setUserData({...userData, genre: "Feminino"})}/>
+                onChange={() => setUserData({ ...userData, genre: "Feminino" })} />
               <Form.Check type="radio" name="genero" label="Masculino" inline required
-                onChange={() => setUserData({...userData, genre: "Masculino"})}/>
+                onChange={() => setUserData({ ...userData, genre: "Masculino" })} />
               <Button type="submit" className="d-block mt-4 w-100">
                 <i className="bi bi-check2-circle me-2"></i>
                 Enviar
@@ -121,7 +121,7 @@ function App() {
         </Modal>
         <Row className="py-3">
           {users?.map(item => (
-            <Col md={4} key={item._id}>
+            <Col md={6} lg={4} key={item._id}>
               <Card className="mb-3 shadow">
                 <Card.Img src={item.photoUrl} />
                 <Card.Body>
@@ -145,7 +145,7 @@ function App() {
         </Row>
       </Container>
       <div className="bg-dark text-white py-3 text-center">
-        <h1>&copy; Anderson Portes - 2022</h1>
+        <h4>&copy; Anderson Portes - 2022</h4>
       </div>
     </div>
   );
